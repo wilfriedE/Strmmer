36f195957ef7138d31dd8d28cf6acc410dff9d3d
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-01-06 01:23:09 -0500

    Base Project

---
+++ Readme.md
@@ -0,0 +1,13 @@
+Strmmer is a python flask based video streaming app.
+
+Why?
+ This is just part of winter break learning project.
+
+Contributions: All contributions welcome.
+
+Playing with Strmmer:
+	- There are two parts to the project.
+	* Simple Streamer
+		- This portion contains the basic concept behind the project which is stream video over http securely.
+	* Strmmer
+		- This is an ongoing implementation of a much more secure approach with basic securities.
---
+++ SimpleStrmmer/main.py
@@ -0,0 +1,38 @@
+# main.py
+from flask import Flask, render_template, stream_with_context, Response
+from camera import VideoCamera
+import base64
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	"""
+	Renders the main index page which includes the video using the stream
+	"""
+	return render_template('index.html')
+
+def feedStream(video,chunksize=10):
+	"""
+	feedStream streams chunks of the video
+	"""
+	with open(video, "rb") as video_file:
+		while True:
+			chunk = video_file.read(chunksize)
+			if chunk:
+				yield chunk
+			else:
+				break
+
+@app.route('/video_feed')
+def video_feed():
+	"""
+	you could add algorithm above this portion to prevent users
+	who shouldn't view the video from viewing it. 
+	This could also have algos for managing expired streams.
+	"""
+	video = "videos/snap-122203822.mp4"
+	return Response(stream_with_context(feedStream(video)))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5001)
